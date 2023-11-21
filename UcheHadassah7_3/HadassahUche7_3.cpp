4b89f36908cbd480b770e09347a4b4989ec0e7c7
Hadassah Uche <uhadassah1@gmail.com>
2023-11-21 15:33:13 +0200

    Add project files.

---
+++ UcheHadassah7_3/HadassahUche7_3.cpp
@@ -0,0 +1,9 @@
+/*a) Using the data type declared for question (2) above, write a program that 
+interactively accepts data into an array of 5 Book structures. Once the data have been 
+entered, the program should list the details of all books.
+b) Write a function that takes as parameters an array of book structures and the array 
+size, and returns the index position of the most expensive book (no printing is done in 
+this function). Test this function in the main program by printing the book details of 
+the most expensive book.
+c) Write a function that takes as parameters an array of book structures and the array 
+size, and displays all the details of all the books in the array. Test your function*/
